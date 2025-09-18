ee866b937bd5cb43f4057d965f7a23a757e4a694
WonyoungJung <yesdino06@gmail.com>
2025-09-18 15:52:44 +0900

    2번쨰 쉬는 시간

---
+++ java-1/src/Foo.java
@@ -0,0 +1,15 @@
+public class Foo {
+    public static void main(String[] args) {
+        var price = 200;
+        var name = "Foo";
+        var pi = 3.14;
+        System.out.println("price = " + (price+1000) + "\n");
+        System.out.println("name = " + name);
+        System.out.println("pi = " + (pi*100));
+        
+        
+        System.out.printf("price = %d\n", (pi*100));
+
+        
+    }
+}
